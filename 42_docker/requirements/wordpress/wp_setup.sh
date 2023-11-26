b0a4487c1a1abea8bf2933b707312953cf2e9cf2
AliHaine <alitr57120@gmail.com>
2023-11-26 20:12:03 +0100

    need to be end mariadb

---
+++ 42_docker/requirements/wordpress/wp_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
+
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
+fi
+
+exec "$@"
