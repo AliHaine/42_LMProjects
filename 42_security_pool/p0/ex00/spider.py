e08f9c26d11c5b55a684481232d27e587de470d7
AliHaine <alitr57120@gmail.com>
2024-10-24 20:54:57 +0200

    Ex00 update to handle custom folder, start ex01

--- 42_security_pool/p0/ex00/spider.py
+++ 42_security_pool/p0/ex00/spider.py
@@ -2,19 +2,53 @@ import sys
 import re
 import requests
 import os
+import shutil
 from bs4 import BeautifulSoup
 
+args = sys.argv
 
-url = "https://42.fr/en/homepage/"
-directory = "data/"
 
-os.mkdir(directory)
+def args_checker():
+    if len(args) <= 1:
+        print("No args provided.")
+        exit(1)
 
-response = requests.get(url)
 
-soup = BeautifulSoup(response.text, 'html.parser')
+def directory_setup():
+    directory = "data/"
+
+    for index, arg in enumerate(args):
+        if arg == "-p":
+            try:
+                directory = args[index + 1]
+            except IndexError:
+                print("No folder provided for the flag -p.")
+                args.pop(index)
+                break
+            del args[index:index + 2]
+            break
+
+    if os.path.isdir(directory) is True:
+        shutil.rmtree(directory)
+    os.mkdir(directory)
+    os.chdir(directory)
+
+
+args_checker()
+
+url = args.pop()
+print(f"Url provided: {url}")
+
+directory_setup()
+
+url_response = None
+try:
+    url_response = requests.get(url)
+except requests.exceptions.RequestException as e:
+    print(f"Error occured with the URL provided {e}")
+    exit(1)
+soup = BeautifulSoup(url_response.text, 'html.parser')
 img_tags = soup.find_all('img')
-print(img_tags)
 
 urls = [img['src'] for img in img_tags]
 
@@ -22,6 +56,7 @@ for url in urls:
     filename = re.search(r'/([\w_-]+[.](jpg|jpeg|bmp|gif|png))$', url)
     if not filename:
         continue
-    with open(directory + filename.group(1), mode='wb') as f:
+    print(f"Download {filename.group(1)}")
+    with open(filename.group(1), mode='wb') as f:
         response = requests.get(url)
         f.write(response.content)
---
+++ 42_security_pool/p0/ex01/scorpion.py
@@ -0,0 +1,13 @@
+from PIL import Image, ExifTags
+
+img = Image.open("a.jpg")
+img_exif = img.getexif()
+
+if img_exif is None:
+    print('Sorry, image has no exif data.')
+else:
+    for key, val in img_exif.items():
+        if key in ExifTags.TAGS:
+            print(f'{ExifTags.TAGS[key]}:{val}')
+        else:
+            print(f'{key}:{val}')
